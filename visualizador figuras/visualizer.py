6f0e43139684bf5c20a114164d073ba65b22fe2a
Jonathan David Rodriguez Lozada <102917544+jonathandrl24@users.noreply.github.com>
2025-01-13 17:52:08 -0500

    vizualizer update

--- visualizador figuras/visualizer.py
+++ visualizador figuras/visualizer.py
@@ -1,19 +1,48 @@
-import turtle as l 
+import turtle as l
 import colorsys
+import time
 
 l.bgcolor("black")
-l.tracer(100)
+l.tracer(0)  
 l.pensize(1)
-h = 0.5
-for i in range(450):
-    c= colorsys.hsv_to_rgb(h,1,1)
-    h= 0.0008
-    l.fillcolor(c)
+
+h = 0.0  
+i = 1  
+increment = 1  
+start_time = time.time()  
+
+while True:  
+    if increment == -1:
+        l.clear()
+        l.bgcolor("black") 
+    
+    c = colorsys.hsv_to_rgb(h, 1, 1)
+    
+    l.fillcolor(c)  
     l.begin_fill()
     l.fd(i)
     l.lt(100)
     l.circle(30)
     for j in range(2):
-        l.fd(i*j)
+        l.fd(i * j)
         l.rt(109)
     l.end_fill()
+    
+    h += 0.005 * increment  
+    if h > 1:  
+        h -= 1
+    elif h < 0:  
+        h += 1
+    
+    i += increment  
+    
+    elapsed_time = time.time() - start_time
+    if elapsed_time > 20:  
+        increment = -1  
+    if i <= 1:  
+        start_time = time.time()  
+        increment = 1  
+ 
+    l.update()
+    
+    time.sleep(0.01)
